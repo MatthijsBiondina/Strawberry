4b99ebd3127192c440662d81e98fde4ca64d0de7
matt <biondina.matthijs@gmail.com>
2019-09-19 16:49:33 +0200

    qiskit

--- tutorial.py
+++ tutorial.py
@@ -2,6 +2,9 @@ import numpy as np
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.visualization import plot_histogram
 
+# Use Aer's qasm_simulator
+simulator = Aer.get_backend('quasm_simulator')
+
 # Initialize variables
 circuit = QuantumCircuit(2, 2)
 
